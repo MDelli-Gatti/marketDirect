53a1b25d70a445b90e95d86d975393ae5a48986a
michaeldelli-gatti <michaelddg@gmail.com>
2016-07-19 12:48:23 -0400

    initial import

---
+++ src/main/java/com/marketDirect/MarketDirectApplication.java
@@ -0,0 +1,12 @@
+package com.marketDirect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDirectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketDirectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/marketDirect/MarketDirectApplicationTests.java
@@ -0,0 +1,18 @@
+package com.marketDirect;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MarketDirectApplication.class)
+@WebAppConfiguration
+public class MarketDirectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
