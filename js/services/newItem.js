259644837ec13fca2080bf90513e2f660c59bc6e
David Ryan Delli-Gatti <Gatti@Davids-MacBook-Pro.local>
2016-08-03 16:19:07 -0400

    delete function works from both profile and shopping list

--- js/services/newItem.js
+++ js/services/newItem.js
@@ -56,6 +56,7 @@ module.exports = function(app) {
                             id: itemId
                         }
                     }).then(function(res) {
+                      window.location.reload();
                         console.log("phase two");
                     }).catch(function(response) {
                         console.log('the end of delete',
